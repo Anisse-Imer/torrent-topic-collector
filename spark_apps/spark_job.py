8a715316f99bf6b09455d2f88c8622823bb7e21b
Anisse-Imer <anissezou@gmail.com>
2025-05-12 16:04:38 +0200

    Spark - test script runned

---
+++ spark_apps/spark_job.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("BasicTest").getOrCreate()
+
+# Simplest possible operation
+rdd = spark.sparkContext.parallelize([1, 2, 3, 4])
+print(rdd.collect())
+
+spark.stop()
